7759412252d2ac5690ddef13d8d72c1ee80e6606
Maxadore <maxime.desnoues@orange.fr>
2024-04-03 19:22:22 +0200

    Player Movements

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5.0f;
+    private Rigidbody2D rb;
+    private Vector2 moveDirection;
+
+    public Joystick joystick;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        ProcessInputs();
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void ProcessInputs()
+    {
+        // Utilise la position du joystick pour définir la direction de mouvement
+        float moveX = joystick.Horizontal;
+        float moveY = joystick.Vertical;
+        moveDirection = new Vector2(moveX, moveY).normalized;
+    }
+
+    void Move()
+    {
+        rb.velocity = new Vector2(moveDirection.x * speed, moveDirection.y * speed);
+    }
+}
